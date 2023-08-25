8361156295643aba99e96752a4310a21047d658b
sandipto-sanyal <sandipto.sanyal@gmail.com>
2023-08-25 11:04:32 +0530

    Update custom_js.js
    Added jslist_to_html utilty

--- static/js/custom_js.js
+++ static/js/custom_js.js
@@ -30,3 +30,17 @@ function downloadCSV() {
     a.download = fileName;
     a.click();
 }
+
+/**
+ * This function converts a list of elements to options with proper html tags 
+ * @param {Array} list_of_elements The list of elements to be converted into options
+ * @returns {string} the list with <optio value> tags attached
+ */
+function jslist_to_html(list_of_elements) {
+  html_to_add = ""
+  for (let i = 0; i < list_of_elements.length; i++) {
+    temp = `<option value='${list_of_elements[i]}'>${list_of_elements[i]}</option>`
+    html_to_add += temp
+  }
+  return html_to_add
+}
